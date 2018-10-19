3a79f4ae8fe3598103ff30f6ea23c1f3af450691
Alexander Baker <alex.baker685@gmail.com>
2018-10-19 19:03:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,4 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["X"," "," "," "," "," "," "," "," "]
-def display_board (board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-display_board(board)
+display_board (board)
